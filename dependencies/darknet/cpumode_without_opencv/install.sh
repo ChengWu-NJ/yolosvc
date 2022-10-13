ed26b79bd0da2be98caa22e26cab8a1cd2e44628
WUCHENG <wu.cheng@iwhalecloud.com>
2022-10-13 15:03:58 +0800

    add darknet lib of cpumode

---
+++ dependencies/darknet/cpumode_without_opencv/install.sh
@@ -0,0 +1,6 @@
+sudo cp darknet /usr/local/bin/
+sudo cp libdarknet.so /usr/lib/libdarknet.so
+### sudo cp include/darknet.h /usr/include/darknet.h
+sudo cp darknet.h /usr/local/include/darknet.h
+sudo ldconfig
+
